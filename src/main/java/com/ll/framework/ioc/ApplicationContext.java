0e2c1d3c3759ff8c9c3099284747d97cc434953f
0-0v <bhin.dev.v@gmail.com>
2026-05-03 01:11:28 +0900

    feat: TestFacadePostService에 TestPostService와 TestPostRepository 주입 구현

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,6 +1,7 @@
 package com.ll.framework.ioc;
 
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
 import java.util.HashMap;
@@ -9,15 +10,19 @@ import java.util.Map;
 public class ApplicationContext {
     private final TestPostService testPostService;
     private final TestPostRepository testPostRepository;
+    private final TestFacadePostService testFacadePostService;
     private final Map<String, Object> beans = new HashMap<>();
 
     public ApplicationContext() {
 
         testPostRepository = new TestPostRepository();
         testPostService = new TestPostService(testPostRepository);
+        testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
+
 
         beans.put("testPostRepository", testPostRepository);
         beans.put("testPostService", testPostService);
+        beans.put("testFacadePostService", testFacadePostService);
     }
 
 //    public <T> T genBean(String beanName) {
