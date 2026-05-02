a0524a7ccd97e3bb3b236fbefa2ebc8539098f62
0-0v <bhin.dev.v@gmail.com>
2026-05-03 00:54:10 +0900

    feat: testPostService 빈 생성 및 싱글톤 적용

---
+++ README.md
@@ -0,0 +1,8 @@
+### 🧪 테스트 체크리스트
+
+- [ ] ApplicationContext 객체 생성 확인
+- [ ] testPostService 빈 생성 여부 확인
+- [ ] testPostService 싱글톤 검증
+- [ ] testPostRepository 빈 생성 여부 확인
+- [ ] testPostService → testPostRepository 의존성 주입 여부 확인
+- [ ] testFacadePostService → testPostService, testPostRepository 의존성 주입 여부 확인
--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,21 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
 public class ApplicationContext {
+    private final TestPostService testPostService;
+    private final TestPostRepository testPostRepository;
     public ApplicationContext() {
 
+        testPostRepository = new TestPostRepository();
+        testPostService = new TestPostService(testPostRepository);
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        if(beanName.equals("testPostService")){
+            return (T) testPostService;
+        }
+        return null;
     }
 }
